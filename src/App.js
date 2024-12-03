fb5914f568d9dfc38bd8964d243e72cba41411a7
danilyasenov <yasenov_d@auca.kg>
2024-12-03 13:03:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/Button"
+
+function App(){
+  return (
+      <div>
+        <Button variant="bordered">Confirm</Button>
+        <Button>Cancel</Button>
+      </div>
+  );
+}
+
+export default App;
