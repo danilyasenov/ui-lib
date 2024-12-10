e5377170c20809221ef9d8dd8b032bd548267616
danilyasenov <yasenov_d@auca.kg>
2024-12-10 12:30:50 +0600

    asd

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Button from "./components/Button"
 function App(){
   return (
       <div>
-        <Button variant="bordered">Confirm</Button>
-        <Button>Cancel</Button>
+        <div>
+        <Button variant="bordered" size="sm">Confirm</Button>
+        <Button variant = "bordered" size = "md">Confirm</Button>
+        <Button variant="bordered" size="lg">Confirm</Button>
+        </div>
+        <Button size="sm">Cancel</Button>
+        <Button size = "md">Cancel</Button>
+        <Button size="lg">Cancel</Button>
       </div>
   );
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,8 @@
 import "./index.css"
 
 function Button(props){
-    const {variant = "primary",children} = props;
-    const className = `my-button my-button--${variant}`;
-
+    const {variant = "primary",size = "md",children} = props;
+    const className = `my-button my-button--${variant} my-button--${size}`;
 
     return <button className={className}>{children}</button>
         
